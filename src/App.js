3dcede33e29fa392a743cf70020f9f9a14bc2aa9
Had1na <gadubas@gmail.com>
2024-10-02 13:19:41 +0100

    Added github link

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ function App() {
       <header className="App-header">
         <h1>Weather App</h1>
         <Weather />
+        <p>
+          {" "}
+          This project was coded by Gabriella Dubas and is open sourced on{" "}
+          <a
+            href="https://github.com/Had1na/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </p>
       </header>
     </div>
   );
